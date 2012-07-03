f03f2473349b6fc44239a243fed54a9734a3bac9
Fang Nan <nanfang05@gmail.com>
2012-07-03 23:19:15 +0800

    create project

---
+++ PackingCheck/PackingCheck/PCKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCKAppDelegate.h
+//  PackingCheck
+//
+//  Created by nanfang on 7/3/12.
+//  Copyright (c) 2012 tukeQ.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
